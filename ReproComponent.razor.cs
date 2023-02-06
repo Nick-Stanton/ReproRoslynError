79abb6f7a972880fb5636c8c1510f6a278489d25
nickstanton <nickstanton@DESKTOP-MBIJHEL>
2023-02-06 14:15:13 -0700

    Add project files.

---
+++ ReproComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace ReproRoslynError
+{
+    public class Component
+    {
+        private IJSObjectReference? _jsModule;
+
+        protected async Task Repro()
+        {
+            _ = _jsModule?.InvokeVoidAsync("repro", "test");
+        }
+    }
+}
